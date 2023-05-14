c7ea83ae431c263d34510cde2ae64af12ff65f1b
Dasusurya403 <129511859+Dasusurya403@users.noreply.github.com>
2023-05-14 23:46:32 +0530

    Add files via upload

---
+++ b.java
@@ -0,0 +1,58 @@
+public static int fibonacci(int number){
+    if(number == 1 || number == 2){ //base case
+        return 1;
+    }
+    return fibonacci(number-1) + fibonacci(number -2); 
+}  
+public static void main(String args[]) {
+    int number = new Scanner(System.in).nextInt();
+    for(int i=1; i<=number; i++){
+        System.out.print(fibonacci2(i) +" ");
+    }    
+}
+
+public static int fibonacci(int number){
+    if(number == 1 || number == 2){ //base case
+        return 1;
+    }
+    return fibonacci(number-1) + fibonacci(number -2); 
+}  
+public static void main(String args[]) {
+    int number = new Scanner(System.in).nextInt();
+    for(int i=1; i<=number; i++){
+        System.out.print(fibonacci2(i) +" ");
+    }    
+  
+    return fibonacci(number-1) + fibonacci(number -2); 
+}  
+public static void main(String args[]) {
+    int number = new Scanner(System.in).nextInt();
+    for(int i=1; i<=number; i++){
+        System.out.print(fibonacci2(i) +" ");
+    }    
+}
+}
+public static void main(String args[]) {
+    int number = new Scanner(System.in).nextInt();
+    for(int i=1; i<=number; i++){
+        System.out.print(fibonacci2(i) +" ");.
+          
+          
+    }    
+  
+  }
+public static void main(String args[]) {
+    int number = new Scanner(System.in).nextInt();
+    for(int i=1; i<=number; i++){
+        System.out.print(fibonacci2(i) +" ");.
+          
+          
+    }    
+}
+
+88888888888
+*****************
+************
+'00000000000000
+
+hfuyfjugxkiuxh
